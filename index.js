1635b2c03bf3cb50cec6200842178367b1cecce6
Salehe <salehe.mkomwa@student.moringaschool.com>
2025-03-11 12:07:38 +0300

    initial

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code in this file!
+// Define current user
+const currentUser = 'Grace Hopper';
+
+// Messages
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+// Distance calculation functions
+const headquarters = 42;
+const blockLengthInFeet = 264;
+
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - headquarters);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * blockLengthInFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * blockLengthInFeet;
+}
+
+// Fare calculation function
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
